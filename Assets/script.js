21011e87e4442a4cbffb8820ea9270a3319fe36b
President <aaron.m.wilson@uconn.edu>
2022-06-20 15:57:03 -0400

    added on click event to savebutton that will store info in timeblock to local storage

--- Assets/script.js
+++ Assets/script.js
@@ -2,3 +2,14 @@
 var timeBlock = $(".time-block")
 var saveBtn  = $(".savebtn");
 var timeContainer = $(".container")
+
+//on click event to save contents of timeblocks to local storage
+
+saveBtn.on("click", function() {
+
+    var time = $(this).siblings(".hour").text();
+    var plan = $(this).siblings(".plan").val();
+
+    localStorage.setItem(time, plan);
+    
+});
