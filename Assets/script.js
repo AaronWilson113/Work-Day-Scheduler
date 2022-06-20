74b0b1f240fb986803227683fc127a9ede3d7ffe
President <aaron.m.wilson@uconn.edu>
2022-06-20 18:14:22 -0400

    added function to change color of block depending on hour

--- Assets/script.js
+++ Assets/script.js
@@ -3,6 +3,9 @@ var timeBlock = $(".time-block")
 var saveBtn = $(".saveBtn");
 var timeContainer = $(".container")
 
+//moment.js to display current day
+$("#currentDay").text(moment().format('dddd MMMM Do YYYY'));
+
 
 //on click event to save contents of timeblocks to local storage
 saveBtn.on("click", function() {
@@ -30,9 +33,38 @@ function usePlanner() {
     });
 }
 
+//function to set the color of block depending on hour
+function hourTracker() {
+   
+    var currentHour = moment().hour(); 
+
+  
+    $(".time-block").each(function () {
+        var blockHour = parseInt($(this).attr("id").split("hour")[1]);
+        console.log( blockHour, currentHour)
+
+      
+        if (blockHour < currentHour) {
+            $(this).addClass("past");
+            $(this).removeClass("future");
+            $(this).removeClass("present");
+        }
+        else if (blockHour === currentHour) {
+            $(this).removeClass("past");
+            $(this).addClass("present");
+            $(this).removeClass("future");
+        }
+        else {
+            $(this).removeClass("present");
+            $(this).removeClass("past");
+            $(this).addClass("future");
+        }
+    })
+}
+ 
 
 
 usePlanner();
-
+hourTracker();
 
 
